006ccc037f6b96337c4f2bac326de995ef521160
Michael McNair <michaeltsuyoshi@gmail.com>
2023-02-17 15:40:38 +0900

    initial commit

---
+++ starter/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/hello', (req, res) => {
+  res.status(200).send('Task Manager App');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+app.get('*', (req, res) => {
+  res.status(404).send(`404 page doesn't exist.`);
+});
+
+const PORT = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(PORT, console.log(`server is listening on port ${PORT}...`));
+  } catch (err) {}
+};
+
+start();
